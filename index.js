b691d9f552f9f1d44379692e3a8b5e0f4eba6854
junaid786-prog <junaidshoaib555@gmail.com>
2025-02-26 14:41:40 +0500

    fix: added docker

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const chatSocket = require('./sockets/chatSocket');
 
-const port = 5002;
+const port = 8001;
 
 const server = http.createServer();
 const io = new Server(server, { cors: { origin: "*" } });
